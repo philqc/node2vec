827c22a50f1781430d9350f561455a89211f260a
user01 <user01@mila.quebec>
2019-11-15 21:16:34 +0000

    reduce graph

--- src/linkPred_arxiv.py
+++ src/linkPred_arxiv.py
@@ -3,6 +3,7 @@
 import os
 import pickle
 import pandas as pd
+import numpy as np
 import networkx as nx
 from utils import project_root, ARXIV_EDGE
 import logging
@@ -13,8 +14,21 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 file_edgelist = os.path.join(project_root(), "tests", "data", ARXIV_EDGE)
 
 def create_graph(edge_csv):
-    g =nx.read_edgelist(edge_csv, delimiter=',', create_using=nx.Graph())
+    g  =nx.read_edgelist(edge_csv, delimiter=',', create_using=nx.DiGraph(), encoding='utf-8-sig')
+    for edge in g.edges():
+        g[edge[0]][edge[1]]['weight'] = 1
+    g = g.to_undirected()
     return g
+
+def remove_edges(g, min_edges, portion=0.5):
+    
+    all_edges = g.edges()
+    num_rm_edges = int((g.number_of_edges()-len(min_edges))*portion)
+    np.random.shuffle(all_edges)
+
+
+def min_spanning_edges(g):
+    return list(nx.minimum_spanning_edges(g))
 
 def sample_positive():
     pass
@@ -22,5 +36,6 @@ def sample_positive():
 def sample_negtive():
     pass
 
+
 g = create_graph(file_edgelist)
-print(nx.info(g))
+print(nx.is_connected(g))
