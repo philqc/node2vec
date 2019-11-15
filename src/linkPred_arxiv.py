0d58b89fb0728b4e6c40d1d8b2a72e2d62f49e05
user01 <user01@mila.quebec>
2019-11-15 21:33:01 +0000

    structure of link prediction code

--- src/linkPred_arxiv.py
+++ src/linkPred_arxiv.py
@@ -21,11 +21,18 @@ def create_graph(edge_csv):
     return g
 
 def remove_edges(g, min_edges, portion=0.5):
-    
     all_edges = g.edges()
     num_rm_edges = int((g.number_of_edges()-len(min_edges))*portion)
     np.random.shuffle(all_edges)
-
+    
+    removed_edges = []
+    for edge_uv in all_edges:
+        edge_vu = (edge_uv[1], edge_uv[2])
+        if (edge_uv not in all_edges) and (edge_vu not in all_edges):
+            removed_edges.append((edge_uv[0], edge_uv[1], 1))
+            g.remove_edge(edge_uv[0], edge_uv[1])
+        if len(removed_edges) == num_rm_edges:
+            return g,  removed_edges
 
 def min_spanning_edges(g):
     return list(nx.minimum_spanning_edges(g))
@@ -36,6 +43,25 @@ def sample_positive():
 def sample_negtive():
     pass
 
+def main():
+    #create graph
+    g = create_graph(file_edgelist)
+    print(nx.info(g))
 
-g = create_graph(file_edgelist)
-print(nx.is_connected(g))
+    #reduce the graph
+
+    #convert reduced graph to csv
+
+    #create positive and negative samples
+
+    #run node2vec to get embedding
+
+    #create feature and label matrix with custom binary operator
+
+    #evaluation through AUC score
+    
+
+if __name__ == "__main__":
+    main()
+
+
